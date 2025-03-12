0d1e2dfdffef6f5d7f7eb52d208a2e20269c0f48
Weishan Li <weishan.2.li@cuanschutz.edu>
2025-03-12 12:04:30 -0600

    Added helper function to subset the processed scRNA seq anndata object into cell type specific anndatas to facilitate sampling per cell type

--- buddi/preprocessing/utils.py
+++ buddi/preprocessing/utils.py
@@ -24,3 +24,19 @@ def get_true_proportions(
     """
     prop_counts = in_adata.obs[cell_type_col].value_counts(normalize=True)
     return pd.DataFrame(prop_counts).T  # Return as a single-row DataFrame
+
+def subset_adata_by_cell_type(
+        in_adata: AnnData, 
+        cell_type_col: str
+    ) -> CellDf:
+    """
+    Constructs a dictionary mapping each cell type to a subset of the AnnData object
+    containing only cells of that type.
+    Requires a user specified column name in in_adata.obs that contains cell type labels.
+
+    :param in_adata: The AnnData object containing single-cell expression data.
+    :param cell_type_col: Column name in in_adata.obs specifying the cell type labels.
+    :return: Dictionary where keys are cell type names and values are subsetted AnnData objects.
+    When a cell type is not present in the input data, the corresponding value will be an empty AnnData object.
+    """
+    return {ctype: in_adata[in_adata.obs[cell_type_col] == ctype] for ctype in in_adata.obs[cell_type_col].unique()}
