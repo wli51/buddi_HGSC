0d5c3affd4dadc65e152ec138dbab219c4732e04
Weishan Li <weishan.2.li@cuanschutz.edu>
2025-03-13 12:36:00 -0600

    Removed generate_random_pseudo_bulk_profiles logic to reduce redundancy

--- buddi/preprocessing/generate_pseudo_bulks.py
+++ buddi/preprocessing/generate_pseudo_bulks.py
@@ -98,78 +98,17 @@ def generate_random_pseudo_bulk_profiles(
     :param cell_df: Dictionary mapping cell types to subsetted AnnData objects.
     :param cell_type_col: Column name in in_adata.obs specifying cell type labels.
     :param cell_noise: List of noise vectors for each cell type.
-    :param use_sample_noise: Whether to apply additional sample noise.
-    :param num_test_samples: Number of samples to reserve for the test set.
-    :return: Tuple (total_prop_df, total_expr_df, test_prop_df, test_expr_df).
+    :param use_sample_noise: Whether to apply additional noise to the pseudobulk profiles.
+    :return: Tuple of (total proportion DataFrame, total expression DataFrame).
     """
-    num_celltypes = len(cell_df)
-    cell_order = list(cell_df.keys())
+    
+    count_df = utils.generate_count_from_props(props_df, num_cells)
 
-    # Generate cell-specific noise if not provided
-    if cell_noise is None:
-        cell_noise = [np.random.lognormal(0, 0.1, in_adata.shape[1]) for _ in range(num_celltypes)]
-
-    total_prop_list, total_expr_list = [], []
-    test_prop_list, test_expr_list = [], []
-
-    for samp_idx in range(num_samples + num_test_samples):  # Last num_test_samples for test set
-        
-        # TODO: replace with formal logging or some progress bar
-        if samp_idx % 100 == 0:
-            print(f"Processing sample {samp_idx}")
-
-        n_cells = num_cells if num_cells is not None else np.random.randint(200, 5000)
-
-        # Generate proportion vector (true or lognormal)
-        if use_true_prop:
-            count_vec = utils.generate_true_counts(in_adata, n_cells, cell_type_col)
-        else:
-            count_vec = utils.generate_log_normal_counts(num_celltypes, n_cells)
-
-        # Convert to DataFrame
-        props_df = pd.DataFrame([count_vec], columns=cell_order)
-        props_df = props_df.div(props_df.sum(axis=1), axis=0)
-
-        # Initialize pseudobulk expression
-        sum_over_cells = np.zeros(in_adata.shape[1])
-
-        for cell_idx, cell_type_id in enumerate(cell_order):
-            num_cell = count_vec[cell_idx]
-            ct_sum = utils.get_cell_type_sum(in_adata, cell_df[cell_type_id], num_cell)
-            ct_sum = np.multiply(ct_sum, cell_noise[cell_idx])
-
-            sum_over_cells += ct_sum
-
-        if use_sample_noise:
-            sample_noise = np.random.lognormal(0, 1, in_adata.shape[1])
-            sum_over_cells *= sample_noise
-            sum_over_cells *= np.random.lognormal(0, 0.1, 1)[0]
-            sum_over_cells *= np.random.lognormal(0, 0.1, in_adata.shape[1])
-            sum_over_cells = np.random.poisson(sum_over_cells)[0]
-
-        sum_over_cells_df = pd.DataFrame(sum_over_cells).T
-        sum_over_cells_df.columns = in_adata.var['gene_ids']
-
-        # Assign to training or test set
-        if samp_idx < num_samples:
-            total_prop_list.append(props_df)
-            total_expr_list.append(sum_over_cells_df)
-        else:
-            test_prop_list.append(props_df)
-            test_expr_list.append(sum_over_cells_df)
-
-    prop_df = pd.concat(total_prop_list, axis=0)
-    pseudo_bulk_df = pd.concat(total_expr_list, axis=0)
-
-    if num_test_samples > 0:
-        test_prop_df = pd.concat(test_prop_list, axis=0)
-        test_expr_df = pd.concat(test_expr_list, axis=0)
-    else:
-        test_prop_df, test_expr_df = None, None
-
-    return (
-        prop_df, 
-        pseudo_bulk_df, 
-        test_prop_df, 
-        test_expr_df
+    return generate_pseudo_bulk_from_counts(
+        in_adata=in_adata, 
+        cell_df=cell_df, 
+        count_df=count_df,
+        cell_noise=cell_noise, 
+        use_sample_noise=use_sample_noise,
+        sample_noise_kwargs=sample_noise_kwargs
     )
